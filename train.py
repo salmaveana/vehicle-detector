c6244f0baafe9bb9f4d14536aa9d2f3dc1a6b924
Salma Huerta <salmaveana@gmail.com>
2021-10-25 20:38:54 -0500

    Update creation of output files

--- train.py
+++ train.py
@@ -1,6 +1,11 @@
 # USAGE
 # python train.py --dataset path/to/dataset
 
+# Enable/disable debugging logs (0,1,2,3)
+# 0 -> all, 3 -> none
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 # Import scikit-learn Libraries
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.preprocessing import MultiLabelBinarizer
@@ -19,6 +24,8 @@ from cnn.datasets import SimpleDatasetLoader
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.utils import to_categorical
 from imutils import paths
+from pathlib import Path
+import pandas
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -26,14 +33,35 @@ import argparse
 import pickle
 
 # Modify parameters
-LR = 0.005
+TEST = 1
+DATASET = "tipo_v4"
+WIDTH = 128
+HEIGHT = 128
+TEST_SIZE = 0.1
+LR = 0.003
 BS = 32
 EPOCHS = 100
 VERBOSE = 10
-MODEL_PATH = "output/car.h5"
-MLB_PATH = "output/car.pickle"
-CM_PATH = "output/car_cm.png"
-PLOT_PATH = "output/car_plot.png"
+
+# LR to exponential
+lr = "{:e}".format(LR)
+
+# Create output strings
+model_path = str(TEST) + "_model_" + DATASET + "_" + str(WIDTH) + "_" + str(int(TEST_SIZE*100)) +  ".h5"
+mlbi_path = str(TEST) + "_mlbi_" + DATASET + "_" + str(WIDTH) + "_" + str(int(TEST_SIZE*100)) + ".pickle"
+cmat_path = str(TEST) + "_cmat_" + DATASET + "_" + str(WIDTH) + "_" + str(int(TEST_SIZE*100)) + ".png"
+plot_path = str(TEST) + "_plot_" + DATASET + "_" + str(WIDTH) + "_" + str(int(TEST_SIZE*100)) + ".png"
+pred_path = str(TEST) + "_pred_" + DATASET + "_" + str(WIDTH) + "_" + str(int(TEST_SIZE*100)) + ".csv"
+
+# Configure output dir
+BASE_OUTPUT = "output/" + DATASET + "/"
+Path(BASE_OUTPUT).mkdir(parents=True, exist_ok=True)
+
+MODEL_PATH = BASE_OUTPUT + model_path
+MLBI_PATH = BASE_OUTPUT + mlbi_path
+CMAT_PATH = BASE_OUTPUT + cmat_path
+PLOT_PATH = BASE_OUTPUT + plot_path
+PRED_PATH = BASE_OUTPUT + pred_path
 
 # Parse the arguments
 ap = argparse.ArgumentParser()
@@ -41,7 +69,7 @@ ap.add_argument("-d", "--dataset", required=True, help="path to input dataset")
 args = vars(ap.parse_args())
 
 # Initialize the image preprocessors
-sp = SimplePreprocessor(32, 32)
+sp = SimplePreprocessor(WIDTH, HEIGHT)
 iap = ImageToArrayPreprocessor()
 
 # Load images
@@ -66,7 +94,7 @@ for (i, label) in enumerate(mlb.classes_):
 
 # Partition data into training and testing 
 # 75% of data for training, 25% of data for testing
-(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.25, random_state=42)
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=TEST_SIZE, random_state=42)
 
 # Convert the labels from integers to vectors
 #trainY = LabelBinarizer().fit_transform(trainY)
@@ -80,7 +108,7 @@ if len(mlb.classes_) == 2:
 # Initialize the optimizer and model
 print("[INFO] compiling model...")
 opt = SGD(learning_rate=LR)
-model = SmallerVGGNet.build(width=32, height=32, depth=3, classes=2)
+model = SmallerVGGNet.build(width=WIDTH, height=HEIGHT, depth=3, classes=2)
 model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
 # Print network summary
@@ -99,14 +127,18 @@ model.save(MODEL_PATH, save_format="h5")
 
 # Save the multi-label binarizer to disk
 print("[INFO] serializing label binarizer...")
-f = open(MLB_PATH, "wb")
+f = open(MLBI_PATH, "wb")
 f.write(pickle.dumps(mlb))
 f.close()
 
 # Evaluate the network
 print("[INFO] evaluating network...")
 predictions = model.predict(testX, batch_size=32)
-print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=["sedan", "suv"]))
+report = classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=["camioneta", "sedan"], output_dict=True)
+print(report)
+report_dict = classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=["camioneta", "sedan"], output_dict=True)
+df = pandas.DataFrame(report_dict).transpose()
+df.to_csv(PRED_PATH)
 
 # Create Confusion Matrix
 print("\n[INFO] Confusion Matrix...")
@@ -119,17 +151,17 @@ sns.heatmap(cm, annot=True, ax=ax, fmt='g', cmap='Greens')  # annot=True to anno
 ax.set_xlabel('Predicted labels')
 ax.set_ylabel('True labels')
 ax.set_title('Confusion Matrix')
-ax.xaxis.set_ticklabels(['sedan', 'suv'])
-ax.yaxis.set_ticklabels(['sedan', 'suv'])
-plt.savefig(CM_PATH)
+ax.xaxis.set_ticklabels(['camioneta', 'sedan'])
+ax.yaxis.set_ticklabels(['camioneta', 'sedan'])
+plt.savefig(CMAT_PATH)
 
 # Plot training loss and accuracy
 plt.style.use("ggplot")
 plt.figure()
-plt.plot(np.arange(0, 100), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, 100), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, 100), H.history["accuracy"], label="train_acc")
-plt.plot(np.arange(0, 100), H.history["val_accuracy"], label="val_acc")
+plt.plot(np.arange(0, EPOCHS), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, EPOCHS), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, EPOCHS), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, EPOCHS), H.history["val_accuracy"], label="val_acc")
 plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
